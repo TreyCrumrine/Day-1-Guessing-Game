bd02a6dd9619c4888e3fbb53239dbb60425ce383
unknown <tcrumrine51@gmail.com>
2023-01-11 14:44:01 -0500

    Wrote more Code

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome everyone to Mat and Trey's Guessing Game!!");
 
-int guessNumber = 6;
+int correctNumber = 6;
+
+
+Console.WriteLine("Guess a number between 1 and 10!");
+int guessNumber = Convert.ToInt32(Console.ReadLine());
+
+if (guessNumber == correctNumber)
+{
+    Console.WriteLine("You guessed the secret number!");
+}
+else
+{
+    Console.WriteLine("You chose poorly");
+}
+
